44380a8470904a668fdc7be22f54e82da89c82ac
clofresh <clofresh@users.noreply.github.com>
2026-04-04 04:50:13 +0000

    Remove preview for PR 2 🛬

--- pr-preview/pr-2/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sullytear/pr-preview/pr-2/sw.js', { scope: '/sullytear/pr-preview/pr-2/' })})}
